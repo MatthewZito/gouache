6cfe76aa7bb31c52b4833eae6a26ebadc69d285a
Matthew Zito <matthewtzito@gmail.com>
2022-07-22 09:53:02 -0500

    test(premux): add middleware unit tests

--- premux/middleware_test.go
+++ premux/middleware_test.go
@@ -3,7 +3,77 @@ package premux
 import (
 	"fmt"
 	"net/http"
+	"net/url"
+	"reflect"
+	"testing"
 )
+
+func TestNewMiddleware(t *testing.T) {
+	actual := newMiddlewares(nil)
+	var expected middlewares
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("expected %v but got %v\n", actual, expected)
+	}
+}
+
+func TestMiddlewareInvocation(t *testing.T) {
+	r := NewRouter()
+
+	r.WithMethods(http.MethodGet).Handler("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "/")
+	})).Use(first, second, third).Register()
+
+	r.WithMethods(http.MethodPost).Handler("/foo", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "foo")
+	})).Use(first, second).Register()
+
+	r.WithMethods(http.MethodPost).Handler("/foo/:id", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "foo")
+	})).Use(func(next http.Handler) http.Handler {
+		// Test access to context params
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "first: before\n")
+
+			id := GetParam(r.Context(), "id")
+			fmt.Fprintf(w, "%v\n", id)
+
+			url, _ := url.Parse(r.URL.RawQuery)
+			values := url.Query()
+
+			values.Add("test", "testval")
+			url.RawQuery = values.Encode()
+
+			next.ServeHTTP(w, r)
+			fmt.Fprintf(w, "first: after\n")
+		})
+	}, second).Register()
+
+	tests := []testCase{
+		{
+			path:   "/",
+			method: http.MethodGet,
+			code:   http.StatusOK,
+			body:   "first: before\nsecond: before\nthird: before\n/third: after\nsecond: after\nfirst: after\n",
+		},
+		{
+			path:   "/foo",
+			method: http.MethodPost,
+			code:   http.StatusOK,
+			body:   "first: before\nsecond: before\nfoosecond: after\nfirst: after\n",
+		},
+		{
+			path:   "/foo/20",
+			method: http.MethodPost,
+			code:   http.StatusOK,
+			body:   "first: before\n20\nsecond: before\nfoosecond: after\nfirst: after\n",
+		},
+	}
+
+	if err := runHTTPTests(r, tests); err != nil {
+		t.Error(err)
+	}
+}
 
 func first(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
